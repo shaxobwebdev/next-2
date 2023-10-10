03619c7708ee6f840172b8b7f453070cd61eaae2
shaxobwebdev <shahobiddindavlatov@gmail.com>
2023-10-11 00:06:07 +0500

    salom

---
+++ main.js
@@ -0,0 +1,142 @@
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// let res2 = (item) => {
+//   let filtered = arr.filter((el) => el != item);
+//   return filtered;
+// };
+
+// console.log(arr);
+
+// let arr = [20, 43, 12, 34, 44];
+
+// let res = arr.reduce((item, total) => item * total, 1);
+
+// let ages = [20, 43, 12, 34, 15];
+
+// console.log(arr);
+
+/// 1 masala
+
+// let combined = ages
+//   .map((item) => item)
+
+//   .sort((a, b) => a - b)
+//   .filter((item) => item != 12)
+//   .reduce((el, total) => el + total, 0);
+
+//   // 2masala
+
+// let maxSpeed = 20;
+
+// maxSpeed = 45;
+
+// console.log(maxSpeed);
+
+// let maxspeed = [20, 30, 50, 60];
+
+// maxspeed = 100;
+
+// console.log(maxspeed);
+
+// 3 masala
+
+// let age = 24;
+
+// let res = age > 24 ? "hello" : "bye";
+
+// let age = 24; // date type = number
+
+// let myName = "shahob"; // date-type = string
+
+// let bool = false; // date-type  = boolean
+
+// let number; //date- type = undefined
+
+// console.log(number);
+
+// object
+
+// let person = {
+//   name: "shaxob", //string
+
+//   age: 25, // number
+
+//   isMarried: false, //boolean
+// };
+
+// console.log(person);
+
+// // 4 masala
+
+// // // array
+
+// let arr = ["name", "age", "isMarried"];
+
+// console.log(arr[1]);
+// arr[0] = "Desktop";
+// console.log(arr);
+
+// misollar yechish
+
+// let x = 5;
+// let y = 6;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log((x = y));
+
+// let z = 10;
+
+// console.log(z - 10);
+
+// x++;
+
+// console.log(x);
+
+// x--;
+
+// console.log(x);
+
+// let a = 10;
+// let b = 10;
+
+// console.log((a = b));
+
+// console.log("10" === 10);
+
+// console.log(-4 / 0);
+
+// console.log("4" + 4);
+
+// alert("hello world!");
+
+// let answers = confirm("yoshingiz nechida");
+
+// console.log(answers);
+
+// let answers = prompt("What is your name?");
+
+// console.log(answers);
+
+//masala---11
+
+// function greet(name) {
+//   ["hello" + "shahob" + "Nechi yoshdasiz"];
+// }
+
+// console.log(greet);
+
+// let ansvers = prompt("qayerdansiz");
+// console.log(ansvers);
+
+// let x = 1997;
+// let y = 2023;
+
+// console.log(x === y);
+
+// function sumStr(a, b) {
+//   return a + b;
+// }
+
+// console.log(sumStr);
